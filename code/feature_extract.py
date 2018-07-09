7cf2f7e3b05d80db1fa0b3e20230da7c9d1cb575
Ninad Khargonkar <ninad.khargonkar@gmail.com>
2018-07-09 14:02:17 -0400

    optimization module

--- code/feature_extract.py
+++ code/feature_extract.py
@@ -1,22 +1,13 @@
 from __future__ import division
 import operator
 import numpy as np
-from collections import defaultdict
 
 from pyitlib import discrete_random_variable as drv
 from data_loading import load_data
 
-filePath = '../data/Age50_DataExtract.csv'
 EST = 'JAMES-STEIN'
-K = 5
-data_arr = load_data(filePath)
-N = data_arr.shape[0]
-num_rand = data_arr.shape[1]
 
-
-
-def discrete_Lmeasure(data_arr):
-    
+def discrete_Lmeasure(data_arr, EST):    
     indi_entropies = drv.entropy(data_arr.T, estimator=EST)
     num_rand = data_arr.shape[1]    
     assert num_rand == len(indi_entropies)
@@ -46,7 +37,6 @@ def discrete_Lmeasure(data_arr):
 # Specifically they are ith and jth columns in the data 2-d array
 # assuming 0-indexing
 def discrete_mu(data_arr, i , j):
-    
     N = data_arr.shape[0]
     set_xi = set(data_arr[:,i])
     set_yj = set(data_arr[:,j])
@@ -68,8 +58,8 @@ def discrete_mu(data_arr, i , j):
     return mu_sum
 
 
-def discrete_normalized_Lmeasure(data_arr):
-
+def discrete_normalized_Lmeasure(data_arr, EST):
+    # TAKE note: the function expects the array to be in a transpose form
     indi_entropies = drv.entropy(data_arr.T, estimator=EST)
     num_rand = data_arr.shape[1]
     assert num_rand == len(indi_entropies)
@@ -106,13 +96,13 @@ def discrete_normalized_Lmeasure(data_arr):
 
 
 
-def topK_feats(L_measure_dict, K):    
-
+def topK_feats(L_measure_dict, data_arr, K):    
     sorted_list = sorted(L_measure_dict.items(), key=operator.itemgetter(1), reverse=True)
     # each entry is a tuple of (key, value). We just want the keys
-    topK_keys = [item[0] for item in sorted_list[:K]]    
-
+    topK_keys = [item[0] for item in sorted_list[:K]]        
     val_dict = {}
+    N = data_arr.shape[0]
+
     for k_tuple in topK_keys:
         i = k_tuple[0]
         j = k_tuple[1]    
@@ -139,6 +129,7 @@ def topK_feats(L_measure_dict, K):
 
     return val_dict
 
-lms, _, _ = discrete_normalized_Lmeasure(data_arr)
-vd = topK_feats(lms, 5)
 
+
+# Make it into a class so that data array is shared amongst all the methods
+# Things like EST can also be set like that
--- code/opti.py
+++ code/opti.py
@@ -1,4 +1,39 @@
+from __future__ import division
+import operator
 import numpy as np
-
 from pyitlib import discrete_random_variable as drv
+
 from data_loading import load_data
+from feature_extract import discrete_normalized_Lmeasure, topK_feats
+
+filePath = '../data/Age50_DataExtract.csv'
+EST = 'JAMES-STEIN'
+K = 5
+data_arr = load_data(filePath)
+N = data_arr.shape[0]
+
+lms, _, _ = discrete_normalized_Lmeasure(data_arr, EST)
+vd = topK_feats(lms, data_arr, K)
+
+# theta vector len = num_features (for marginals) + K
+
+# topK_pairs == vd
+def compute_hr(thetas, rvec, topK_pairs):    
+    constraint_sum = 0.0
+    
+    # Can do this before too
+    topK_list = [(k,v) for k,v in topK_pairs.items()]
+    
+    # marginals    
+    num_feats = len(rvec)
+    assert len(rvec) == (len(thetas) - len(topK_list))
+    # CHECKING WITH 1 since BINARY FEATURES
+    for i in range(num_feats):
+        indicator = 1 if rvec[i] == 1 else 0
+        constraint_sum += thetas[i] * indicator
+    
+    # top K constraints
+
+    
+
+    return
