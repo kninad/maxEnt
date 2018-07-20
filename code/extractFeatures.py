fixed normalization constant calculation and optimizer convergence

code/extractFeatures.py
@@ -39,7 +39,8 @@ class ExtractFeatures(object):
                                                 estimator=self.ent_estimator)
                 
                 # TEMP FIX for I(x,y) going negative
-                I_ij = np.abs(I_ij)    
+                # I_ij = np.abs(I_ij)
+                I_ij = np.max(I_ij, 0)    
                 
                 W_ij = min(h_i, h_j)
                 inner_term = (-1.0 * 2 * I_ij) / (1 - float(I_ij)/W_ij)
@@ -99,7 +100,8 @@ class ExtractFeatures(object):
                 
                 # Potential error: I_ij may come out negative depending on the estiamtor   
                 I_ij_hat = I_ij - mu_ij
-                I_ij_hat = np.abs(I_ij_hat)
+                # I_ij_hat = np.abs(I_ij_hat)
+                I_ij_hat = np.max(I_ij_hat, 0)
                 
                 W_ij_hat = W_ij - mu_ij
 

code/optimizer.py
@@ -1,4 +1,6 @@
 from __future__ import division
+import itertools
+
 import numpy as np
 from scipy.optimize import fmin_l_bfgs_b as spmin_LBFGSB
 
@@ -29,7 +31,9 @@ class Optimizer(object):
 
     # This function computes the inner sum of the 
     # optimization function objective
-    def compute_hr(self, thetas, rvec):    
+    
+    # could split thetas into marginal and specials
+    def compute_constraint_sum(self, thetas, rvec):    
         constraint_sum = 0.0
         topK_pairs_dict = self.featsObj.feats_pairs_dict
 
@@ -57,6 +61,22 @@ class Optimizer(object):
 
         return constraint_sum
 
+    # normalization constant Z(theta)
+    # assuming binary features for now.
+    def binary_norm_constant_Z(self, thetas):
+        norm_sum = 0.0
+        # N = self.featsObj.N        
+        data_arr = self.featsObj.data_arr
+        num_feats = data_arr.shape[1]
+        # lst = map(list, itertools.product([0, 1], repeat=n))
+        all_perms = map(np.array, itertools.product([0, 1], repeat=num_feats))
+        
+        for vec in all_perms:
+            tmp = self.compute_constraint_sum(thetas, vec)
+            norm_sum += np.exp(tmp)
+
+        return norm_sum
+
 
     # Objective for the optimization problem
     # It is a function of thetas
@@ -68,8 +88,11 @@ class Optimizer(object):
         # THIS CAN SPED UP BY EFFICIENT NUMPY OPERATIONS
         for i in range(N):
             rvec = data_arr[i,:]
-            inner_constraint_sum = self.compute_hr(thetas, rvec)
+            inner_constraint_sum = self.compute_constraint_sum(thetas, rvec)
             objective_sum += inner_constraint_sum
+
+        subtraction_term = N * np.log(self.binary_norm_constant_Z(thetas))
+        objective_sum -= subtraction_term
 
         return (-1 * objective_sum) # SINCE MINIMIZING IN THE LBFGS SCIPY FUNCTION
 

code/utils.py
@@ -15,16 +15,21 @@ def load_data(filePath):
 
     return data_arr
 
+# #Index([u'fyAGE', u'CCCfy49', u'CCCfy53', u'CCCfy98', u'CCCfy98.1', u'CCCfy100',
+#        u'CCCfy101', u'CCCfy128', u'CCCfy204', u'CCCfy205', u'CCCfy651',
+#        u'CCCsy49', u'CCCsy53', u'CCCsy98', u'CCCsy100', u'CCCsy101',
+#        u'CCCsy128', u'CCCsy204', u'CCCsy205', u'CCCsy651'],
+#       dtype='object')
 
-def load_data(filePath):
+def load_data_2(filePath):
     df = pd.read_csv(filePath)
     # df1 = df.drop(['fyAGE', 'CCCfy98.1'], axis=1) # erroneous column. CCCfy98 repeated in original csv file
-    tups = [tuple(x) for x in df.values]
+    df = df.drop(['fyAGE', 'CCCfy98.1'], axis=1) 
+    df2 = df.drop(['CCCfy128', 'CCCfy204', 'CCCfy205', 'CCCfy651', 
+                    'CCCsy128', 'CCCsy204', 'CCCsy205', 'CCCsy651'],
+                    axis=1) 
+    tups = [tuple(x) for x in df2.values]
 
     data_arr = np.asarray(tups)
 
     return data_arr
-
-
-
-